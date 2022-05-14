172207e3e38b1d9f1e559e98971835263d1a6657
kelvin <silva.com.br123007@gmail.com>
2022-05-14 14:38:43 -0300

    Menu Finalizado

---
+++ Projeto/main.py
@@ -0,0 +1,22 @@
+from Menu import *
+from time import sleep
+
+while True:
+    MenuPrincipal()
+    while True:
+        try:
+            escolha = int(input("\033[33mSua opção: \033[m"))
+        except:
+            print('\033[31mOcorreu um erro tente novamente\033[m')
+        else:
+            break
+    if escolha == 1:
+        opção1()
+    elif escolha == 2:
+        opção2()
+    elif escolha == 3:
+        print("\033[32mVolte sempre!\033[m")
+        break
+    else:
+        print("\033[31mopção invalida tente novamente!\033[m")
+        sleep(2)
