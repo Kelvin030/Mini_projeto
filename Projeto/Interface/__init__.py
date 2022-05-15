5f1ba461908425b258efa062b0f8ab282b253ae6
kelvin <silva.com.br123007@gmail.com>
2022-05-15 13:04:05 -0300

    v1.0

---
+++ Projeto/Interface/__init__.py
@@ -0,0 +1,20 @@
+def MenuPrincipal():
+    print('-' * 40)
+    print('MENU PRINCIPAL'.center(40))
+    print('-' * 40)
+    print('\033[33m1-\033[m \033[34mVer pessoas cadastradas\033[m')
+    print('\033[33m2-\033[m \033[34mCadastrar nova pessoa\033[m')
+    print('\033[33m3-\033[m \033[34mSair do sistema\033[m')
+    print('-' * 40)
+        
+
+def opção1():
+    print('-' * 40)
+    print('Opção 1'.center(40))
+    print('-' * 40)
+
+
+def opção2():
+    print('-' * 40)
+    print('Opção 2'.center(40))
+    print('-' * 40)
